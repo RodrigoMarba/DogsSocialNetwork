7485220366a9c60d456337946c66d299def49fe1
Rodrigo Marba <marbarodrigo@gmail.com>
2023-08-19 16:59:52 -0300

    Comments section and functionalities

---
+++ src/Components/Photo/PhotoCommentsForm.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { ReactComponent as Send } from "../../Assets/enviar.svg";
+import useFetch from "../../Hooks/useFetch";
+import { COMMENT_POST } from "../../api";
+import Error from "../../Components/Helper/Error";
+import styles from "./PhotoCommentsForm.module.css";
+
+const PhotoCommentsForm = ({ id, setComments }) => {
+	const [comment, setComment] = useState("");
+	const { request, error } = useFetch();
+
+	async function handleSubmit(event) {
+		event.preventDefault();
+		const { url, options } = COMMENT_POST(id, { comment });
+		const { response, json } = await request(url, options);
+		if (response.ok) {
+			setComment("");
+			setComments((comments) => [...comments, json]);
+		}
+	}
+
+	return (
+		<form className={styles.form} onSubmit={handleSubmit}>
+			<textarea
+				className={styles.textarea}
+				id="comment"
+				name="comment"
+				placeholder="Comment"
+				value={comment}
+				onChange={(e) => setComment(e.target.value)}
+			/>
+			<button className={styles.button}>
+				<Send />
+			</button>
+			<Error error={error} />
+		</form>
+	);
+};
+
+export default PhotoCommentsForm;
